86d56d45f318de74956f16c785f231818a29c4b5
Aaron Congo <Aaron.Congo@improving.com>
2023-06-29 17:47:09 -0700

    Split into individual fixtures

--- unit_testing/test_aurora_host_list_provider.py
+++ unit_testing/test_aurora_host_list_provider.py
@@ -12,9 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import collections
 from datetime import datetime, timedelta
-from typing import Tuple
 
 import pytest
 
@@ -33,16 +31,25 @@ def clear_caches():
     AuroraHostListProvider._cluster_ids_to_update.clear()
 
 
+@pytest.fixture(autouse=True)
+def mock_default_behavior(mock_provider_service, mock_conn, mock_cursor):
+    mock_provider_service.current_connection = mock_conn
+    mock_topology_query(mock_conn, mock_cursor, [("new-host-id", True)])
+
+
 @pytest.fixture
-def mocks(mocker):
-    Mocks = collections.namedtuple('Mocks', ['provider_service', 'conn', 'cursor'])
-    conn = mocker.MagicMock()
-    cursor = mocker.MagicMock()
-    provider_service = mocker.MagicMock()
-    provider_service.current_connection = conn
-    mocks = Mocks(provider_service, conn, cursor)
-    mock_topology_query(mocks, [("new-host-id", True)])
-    return mocks
+def mock_conn(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def mock_cursor(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def mock_provider_service(mocker):
+    return mocker.MagicMock()
 
 
 @pytest.fixture
@@ -56,8 +63,8 @@ def initial_hosts():
 
 
 @pytest.fixture
-def cache_hosts() -> Tuple[HostInfo, ...]:
-    return (HostInfo("host1"), HostInfo("host2"),)
+def cache_hosts():
+    return HostInfo("host1"), HostInfo("host2")
 
 
 @pytest.fixture
@@ -70,32 +77,33 @@ def refresh_ns():
     return 5_000_000_000  # 5 seconds
 
 
-def test_get_topology_caches_topology(mocker, mocks, props, cache_hosts, refresh_ns):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_caches_topology(mocker, mock_provider_service, mock_conn, props, cache_hosts, refresh_ns):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     AuroraHostListProvider._topology_cache.put(provider._cluster_id, list(cache_hosts), refresh_ns)
     spy = mocker.spy(provider, "_query_for_topology")
 
-    result = provider.refresh(mocks.conn)
+    result = provider.refresh(mock_conn)
 
     assert cache_hosts == result
     spy.assert_not_called()
 
 
-def test_get_topology_force_update(mocker, mocks, cache_hosts, queried_hosts, props, refresh_ns):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_force_update(
+        mocker, mock_provider_service, mock_conn, cache_hosts, queried_hosts, props, refresh_ns):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     AuroraHostListProvider._topology_cache.put(provider._cluster_id, list(cache_hosts), refresh_ns)
     spy = mocker.spy(provider, "_query_for_topology")
 
-    result = provider.force_refresh(mocks.conn)
+    result = provider.force_refresh(mock_conn)
 
     assert queried_hosts == result
     spy.assert_called_once()
 
 
-def test_get_topology_invalid_dialect(mocker, mocks, initial_hosts, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_invalid_dialect(mocker, mock_provider_service, initial_hosts, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     spy = mocker.spy(provider, "_query_for_topology")
-    mocks.provider_service.dialect = None
+    mock_provider_service.dialect = None
 
     result = provider.refresh()
 
@@ -103,11 +111,12 @@ def test_get_topology_invalid_dialect(mocker, mocks, initial_hosts, props):
     spy.assert_called_once()
 
 
-def test_get_topology_invalid_topology(mocker, mocks, props, cache_hosts, refresh_ns):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_invalid_topology(
+        mocker, mock_provider_service, mock_conn, mock_cursor, props, cache_hosts, refresh_ns):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     AuroraHostListProvider._topology_cache.put(provider._cluster_id, list(cache_hosts), refresh_ns)
     spy = mocker.spy(provider, "_query_for_topology")
-    mock_topology_query(mocks, [("reader", False)])  # Invalid topology: no writer instance
+    mock_topology_query(mock_conn, mock_cursor, [("reader", False)])  # Invalid topology: no writer instance
 
     result = provider.force_refresh()
 
@@ -115,22 +124,22 @@ def test_get_topology_invalid_topology(mocker, mocks, props, cache_hosts, refres
     spy.assert_called_once()
 
 
-def test_get_topology_invalid_query(mocker, mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
-    mocks.cursor.execute.side_effect = ProgrammingError()
+def test_get_topology_invalid_query(mocker, mock_provider_service, mock_conn, mock_cursor, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
+    mock_cursor.execute.side_effect = ProgrammingError()
     spy = mocker.spy(provider, "_query_for_topology")
 
     with pytest.raises(AwsWrapperError):
-        provider.force_refresh(mocks.conn)
+        provider.force_refresh(mock_conn)
     spy.assert_called_once()
 
 
-def test_get_topology_multiple_writers(mocker, mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_multiple_writers(mocker, mock_provider_service, mock_conn, mock_cursor, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     spy = mocker.spy(provider, "_query_for_topology")
     now = datetime.now()
     records = [("old_writer", True, None, None, now), ("new_writer", True, None, None, now + timedelta(seconds=10))]
-    mock_topology_query(mocks, records)
+    mock_topology_query(mock_conn, mock_cursor, records)
 
     result = provider.refresh()
 
@@ -139,11 +148,11 @@ def test_get_topology_multiple_writers(mocker, mocks, props):
     spy.assert_called_once()
 
 
-def test_get_topology_no_connection(mocker, mocks, initial_hosts, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_no_connection(mocker, mock_provider_service, initial_hosts, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     spy = mocker.spy(provider, "_query_for_topology")
-    mocks.provider_service.dialect = None
-    mocks.provider_service.current_connection = None
+    mock_provider_service.dialect = None
+    mock_provider_service.current_connection = None
 
     result = provider.refresh()
 
@@ -151,8 +160,8 @@ def test_get_topology_no_connection(mocker, mocks, initial_hosts, props):
     spy.assert_not_called()
 
 
-def test_create_host_with_provider_uninitialized(mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_create_host_with_provider_uninitialized(mock_provider_service, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
 
     assert provider._cluster_instance_template is None
     with pytest.raises(AwsWrapperError):
@@ -164,18 +173,18 @@ def test_create_host_with_provider_uninitialized(mocks, props):
         provider._create_host(('instance-1',))
 
 
-def test_no_cluster_id_suggestion_for_separate_clusters(mocks):
+def test_no_cluster_id_suggestion_for_separate_clusters(mock_provider_service, mock_conn, mock_cursor):
     props_a = Properties({"host": "instance-A-1.domain.com"})
-    provider_a = AuroraHostListProvider(mocks.provider_service, props_a)
-    mock_topology_query(mocks, [("instance-A-1.domain.com", True)])
+    provider_a = AuroraHostListProvider(mock_provider_service, props_a)
+    mock_topology_query(mock_conn, mock_cursor, [("instance-A-1.domain.com", True)])
     expected_hosts_a = (HostInfo("instance-A-1.domain.com", role=HostRole.WRITER),)
 
     actual_hosts_a = provider_a.refresh()
     assert expected_hosts_a == actual_hosts_a
 
     props_b = Properties({"host": "instance-B-1.domain.com"})
-    provider_b = AuroraHostListProvider(mocks.provider_service, props_b)
-    mock_topology_query(mocks, [("instance-B-1.domain.com", True)])
+    provider_b = AuroraHostListProvider(mock_provider_service, props_b)
+    mock_topology_query(mock_conn, mock_cursor, [("instance-B-1.domain.com", True)])
     expected_hosts_b = (HostInfo("instance-B-1.domain.com", role=HostRole.WRITER),)
 
     actual_hosts_b = provider_b.refresh()
@@ -183,17 +192,17 @@ def test_no_cluster_id_suggestion_for_separate_clusters(mocks):
     assert 2 == len(AuroraHostListProvider._topology_cache)
 
 
-def test_cluster_id_suggestion_for_new_provider_with_cluster_url(mocker, mocks):
+def test_cluster_id_suggestion_for_new_provider_with_cluster_url(mocker, mock_provider_service, mock_conn, mock_cursor):
     props = Properties({"host": "my-cluster.cluster-xyz.us-east-2.rds.amazonaws.com"})
-    provider1 = AuroraHostListProvider(mocks.provider_service, props)
-    mock_topology_query(mocks, [("instance-1", True)])
+    provider1 = AuroraHostListProvider(mock_provider_service, props)
+    mock_topology_query(mock_conn, mock_cursor, [("instance-1", True)])
     expected_hosts = (HostInfo("instance-1.xyz.us-east-2.rds.amazonaws.com", role=HostRole.WRITER),)
 
     actual_hosts = provider1.refresh()
     assert expected_hosts == actual_hosts
     assert provider1._is_primary_cluster_id
 
-    provider2 = AuroraHostListProvider(mocks.provider_service, props)
+    provider2 = AuroraHostListProvider(mock_provider_service, props)
     spy = mocker.spy(provider2, "_query_for_topology")
     provider2._initialize()
 
@@ -206,10 +215,11 @@ def test_cluster_id_suggestion_for_new_provider_with_cluster_url(mocker, mocks):
     spy.assert_not_called()
 
 
-def test_cluster_id_suggestion_for_new_provider_with_instance_url(mocker, mocks):
+def test_cluster_id_suggestion_for_new_provider_with_instance_url(
+        mocker, mock_provider_service, mock_conn, mock_cursor):
     props1 = Properties({"host": "my-cluster.cluster-xyz.us-east-2.rds.amazonaws.com"})
-    provider1 = AuroraHostListProvider(mocks.provider_service, props1)
-    mock_topology_query(mocks, [("instance-1", True)])
+    provider1 = AuroraHostListProvider(mock_provider_service, props1)
+    mock_topology_query(mock_conn, mock_cursor, [("instance-1", True)])
     expected_hosts = (HostInfo("instance-1.xyz.us-east-2.rds.amazonaws.com", role=HostRole.WRITER),)
 
     actual_hosts = provider1.refresh()
@@ -217,7 +227,7 @@ def test_cluster_id_suggestion_for_new_provider_with_instance_url(mocker, mocks)
     assert provider1._is_primary_cluster_id
 
     props2 = Properties({"host": "instance-1.xyz.us-east-2.rds.amazonaws.com"})
-    provider2 = AuroraHostListProvider(mocks.provider_service, props2)
+    provider2 = AuroraHostListProvider(mock_provider_service, props2)
     spy = mocker.spy(provider2, "_query_for_topology")
     provider2._initialize()
 
@@ -230,13 +240,13 @@ def test_cluster_id_suggestion_for_new_provider_with_instance_url(mocker, mocks)
     spy.assert_not_called()
 
 
-def test_cluster_id_suggestion_for_existing_provider(mocker, mocks):
+def test_cluster_id_suggestion_for_existing_provider(mocker, mock_provider_service, mock_conn, mock_cursor):
     props1 = Properties({"host": "instance-2.xyz.us-east-2.rds.amazonaws.com"})
-    provider1 = AuroraHostListProvider(mocks.provider_service, props1)
+    provider1 = AuroraHostListProvider(mock_provider_service, props1)
     records = [("instance-1", False),
                ("instance-2", True),
                ("instance-3", False)]
-    mock_topology_query(mocks, records)
+    mock_topology_query(mock_conn, mock_cursor, records)
     expected_hosts = (HostInfo("instance-1.xyz.us-east-2.rds.amazonaws.com", role=HostRole.READER),
                       HostInfo("instance-2.xyz.us-east-2.rds.amazonaws.com", role=HostRole.WRITER),
                       HostInfo("instance-3.xyz.us-east-2.rds.amazonaws.com", role=HostRole.READER))
@@ -246,7 +256,7 @@ def test_cluster_id_suggestion_for_existing_provider(mocker, mocks):
     assert not provider1._is_primary_cluster_id
 
     props2 = Properties({"host": "my-cluster.cluster-xyz.us-east-2.rds.amazonaws.com"})
-    provider2 = AuroraHostListProvider(mocks.provider_service, props2)
+    provider2 = AuroraHostListProvider(mock_provider_service, props2)
     provider2._initialize()
 
     assert provider2._cluster_id != provider1._cluster_id
@@ -268,108 +278,108 @@ def test_cluster_id_suggestion_for_existing_provider(mocker, mocks):
     spy.assert_not_called()
 
 
-def test_identify_connection_errors(mocks, props):
-    mocks.cursor.fetchone.return_value = None
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_identify_connection_errors(mock_provider_service, mock_conn, mock_cursor, props):
+    mock_cursor.fetchone.return_value = None
+    provider = AuroraHostListProvider(mock_provider_service, props)
 
     with pytest.raises(AwsWrapperError):
-        provider.identify_connection(mocks.conn)
+        provider.identify_connection(mock_conn)
 
-    mocks.cursor.execute.side_effect = Error()
+    mock_cursor.execute.side_effect = Error()
     with pytest.raises(AwsWrapperError):
-        provider.identify_connection(mocks.conn)
+        provider.identify_connection(mock_conn)
 
 
-def test_identify_connection_no_match_in_topology(mocks, props):
-    mocks.cursor.fetchone.return_value = ("non-matching-host",)
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_identify_connection_no_match_in_topology(mock_provider_service, mock_conn, mock_cursor, props):
+    mock_cursor.fetchone.return_value = ("non-matching-host",)
+    provider = AuroraHostListProvider(mock_provider_service, props)
 
-    assert provider.identify_connection(mocks.conn) is None
+    assert provider.identify_connection(mock_conn) is None
 
 
-def test_identify_connection_empty_topology(mocker, mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
-    mocks.cursor.fetchone.return_value = ("instance-1",)
+def test_identify_connection_empty_topology(mocker, mock_provider_service, mock_conn, mock_cursor, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
+    mock_cursor.fetchone.return_value = ("instance-1",)
 
     provider.refresh = mocker.MagicMock(return_value=[])
-    assert provider.identify_connection(mocks.conn) is None
+    assert provider.identify_connection(mock_conn) is None
 
 
-def test_identify_connection_host_in_topology(mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
-    mocks.cursor.fetchone.return_value = ("instance-1",)
-    mock_topology_query(mocks, [("instance-1", True)])
+def test_identify_connection_host_in_topology(mock_provider_service, mock_conn, mock_cursor, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
+    mock_cursor.fetchone.return_value = ("instance-1",)
+    mock_topology_query(mock_conn, mock_cursor, [("instance-1", True)])
 
-    host_info = provider.identify_connection(mocks.conn)
+    host_info = provider.identify_connection(mock_conn)
     assert "instance-1.xyz.us-east-2.rds.amazonaws.com" == host_info.host
     assert "instance-1" == host_info.host_id
 
 
-def test_host_pattern_setting(mocks):
+def test_host_pattern_setting(mock_provider_service, props):
     props = Properties({"host": "127:0:0:1",
                         WrapperProperties.CLUSTER_INSTANCE_HOST_PATTERN.name: "?.custom-domain.com"})
 
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+    provider = AuroraHostListProvider(mock_provider_service, props)
     provider._initialize()
     assert "?.custom-domain.com" == provider._cluster_instance_template.host
 
     with pytest.raises(AwsWrapperError):
         props[WrapperProperties.CLUSTER_INSTANCE_HOST_PATTERN.name] = "invalid_host_pattern"
-        provider = AuroraHostListProvider(mocks.provider_service, props)
+        provider = AuroraHostListProvider(mock_provider_service, props)
         provider._initialize()
 
     with pytest.raises(AwsWrapperError):
         props[WrapperProperties.CLUSTER_INSTANCE_HOST_PATTERN.name] = "?.proxy-xyz.us-east-2.rds.amazonaws.com"
-        provider = AuroraHostListProvider(mocks.provider_service, props)
+        provider = AuroraHostListProvider(mock_provider_service, props)
         provider._initialize()
 
     with pytest.raises(AwsWrapperError):
         props[WrapperProperties.CLUSTER_INSTANCE_HOST_PATTERN.name] = \
             "?.cluster-custom-xyz.us-east-2.rds.amazonaws.com"
-        provider = AuroraHostListProvider(mocks.provider_service, props)
+        provider = AuroraHostListProvider(mock_provider_service, props)
         provider._initialize()
 
 
-def test_get_topology_aware_dialect_invalid_dialect(mocker, mocks, props):
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_topology_aware_dialect_invalid_dialect(mocker, mock_provider_service, props):
+    provider = AuroraHostListProvider(mock_provider_service, props)
     mock_dialect = mocker.MagicMock(spec=Dialect)
-    mocks.provider_service.dialect = mock_dialect
+    mock_provider_service.dialect = mock_dialect
 
     with pytest.raises(AwsWrapperError):
         provider._get_topology_aware_dialect("AuroraHostListProvider.InvalidDialectForGetHostRole")
 
 
-def test_get_host_role(mocks, props):
-    mocks.cursor.fetchone.return_value = (True,)
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+def test_get_host_role(mock_provider_service, mock_conn, mock_cursor, props):
+    mock_cursor.fetchone.return_value = (True,)
+    provider = AuroraHostListProvider(mock_provider_service, props)
 
-    assert HostRole.READER == provider.get_host_role(mocks.conn)
+    assert HostRole.READER == provider.get_host_role(mock_conn)
 
-    mocks.cursor.fetchone.return_value = None
+    mock_cursor.fetchone.return_value = None
     with pytest.raises(AwsWrapperError):
-        provider.get_host_role(mocks.conn)
+        provider.get_host_role(mock_conn)
 
-    mocks.cursor.execute.side_effect = Error()
+    mock_cursor.execute.side_effect = Error()
     with pytest.raises(AwsWrapperError):
-        provider.get_host_role(mocks.conn)
+        provider.get_host_role(mock_conn)
 
 
-def test_cluster_id_setting(mocks):
+def test_cluster_id_setting(mock_provider_service):
     props = Properties({"host": "my-cluster.cluster-xyz.us-east-2.rds.amazonaws.com",
                         WrapperProperties.CLUSTER_ID.name: "my-cluster-id"})
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+    provider = AuroraHostListProvider(mock_provider_service, props)
     provider._initialize()
     assert provider._cluster_id == "my-cluster-id"
 
 
-def test_initialize_rds_proxy(mocks):
+def test_initialize_rds_proxy(mock_provider_service):
     props = Properties({"host": "my-cluster.proxy-xyz.us-east-2.rds.amazonaws.com"})
-    provider = AuroraHostListProvider(mocks.provider_service, props)
+    provider = AuroraHostListProvider(mock_provider_service, props)
     provider._initialize()
     assert provider._cluster_id == "my-cluster.proxy-xyz.us-east-2.rds.amazonaws.com"
 
 
-def mock_topology_query(mocks, records):
-    mocks.conn.cursor.return_value = mocks.cursor
-    mocks.cursor.__enter__.return_value = mocks.cursor  # Mocks out `with conn.cursor() as cursor:`
-    mocks.cursor.__iter__.return_value = records  # Mocks out `for record in cursor:`
+def mock_topology_query(mock_conn, mock_cursor, records):
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.__enter__.return_value = mock_cursor  # Mocks out `with conn.cursor() as cursor:`
+    mock_cursor.__iter__.return_value = records  # Mocks out `for record in cursor:`
