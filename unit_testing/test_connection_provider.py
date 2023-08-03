aac90214bf6c88cf33eb8dcfe80ec21a88ef0d45
Karen <64801825+karenc-bq@users.noreply.github.com>
2023-08-04 06:27:12 +0700

    chore: convert unit tests to pytest (#63)

--- unit_testing/test_connection_provider.py
+++ unit_testing/test_connection_provider.py
@@ -13,147 +13,153 @@
 #  limitations under the License.
 
 
-from unittest import TestCase
-from unittest.mock import MagicMock
-
 import pytest
 
-from aws_wrapper.connection_provider import (ConnectionProvider,
-                                             ConnectionProviderManager,
+from aws_wrapper.connection_provider import (ConnectionProviderManager,
                                              DriverConnectionProvider)
 from aws_wrapper.hostinfo import HostInfo, HostRole
-from aws_wrapper.plugin import CanReleaseResources
 from aws_wrapper.utils.properties import Properties
 
 
-class TestDriverConnectionProvider(TestCase):
-
-    def test_provider_accepts_all_host_infos(self):
-        connection_mock = MagicMock()
-        connection_mock.connect.return_value = "Test"
-        connection_provider = DriverConnectionProvider(connection_mock.connect)
-
-        host_info = HostInfo("localhost")
-        properties = Properties()
-        assert connection_provider.accepts_host_info(host_info, properties) is True
-
-        host_info2 = HostInfo("abc123")
-        assert connection_provider.accepts_host_info(host_info2, properties) is True
-
-    def test_provider_accepts_random_strategy(self):
-        connection_mock = MagicMock()
-        connection_mock.connect.return_value = "Test"
-        connection_provider = DriverConnectionProvider(connection_mock.connect)
-
-        assert connection_provider.accepts_strategy(HostRole.READER, "random") is True
-        assert connection_provider.accepts_strategy(HostRole.READER, "other") is False
-
-    def test_provider_returns_host_info(self):
-        connection_mock = MagicMock()
-        connection_mock.connect.return_value = "Test"
-        connection_provider = DriverConnectionProvider(connection_mock.connect)
-
-        host_info_list = [HostInfo("localhost", role=HostRole.WRITER), HostInfo("other", role=HostRole.READER)]
-        host_info = connection_provider.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
-
-        assert host_info.host == "localhost"
-
-    def test_provider_returns_connection(self):
-        connection_mock = MagicMock()
-        connection_mock.connect.return_value = "Test"
-        connection_provider = DriverConnectionProvider(connection_mock.connect)
-
-        host_info = HostInfo("localhost", 1234)
-        properties = Properties({"test_prop": 5})
-        connection_provider.connect(host_info, properties)
-
-        connection_mock.connect.assert_called_with(test_prop=5, host="localhost", port="1234")
+@pytest.fixture
+def connection_mock(mocker):
+    return mocker.MagicMock()
 
 
-class TestConnectionProviderManager(TestCase):
+@pytest.fixture
+def default_provider_mock(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def set_provider_mock(mocker):
+    return mocker.MagicMock()
+
+
+@pytest.fixture
+def host_info():
+    return HostInfo("localhost")
+
+
+@pytest.fixture
+def properties():
+    return Properties()
+
+
+@pytest.fixture(autouse=True)
+def reset_provider():
+    ConnectionProviderManager._conn_provider = None
+    yield
+    ConnectionProviderManager._conn_provider = None
+
+
+def test_provider_accepts_all_host_infos(connection_mock):
+    connection_mock.connect.return_value = "Test"
+    connection_provider = DriverConnectionProvider(connection_mock.connect)
 
     host_info = HostInfo("localhost")
     properties = Properties()
+    assert connection_provider.accepts_host_info(host_info, properties) is True
 
-    @pytest.fixture(autouse=True)
-    def reset_provider(self):
-        ConnectionProviderManager._conn_provider = None
-        yield
-        ConnectionProviderManager._conn_provider = None
+    host_info2 = HostInfo("abc123")
+    assert connection_provider.accepts_host_info(host_info2, properties) is True
 
-    def test_manager_provides_default_provider(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        provider = connection_provider_manager.get_connection_provider(self.host_info, self.properties)
+def test_provider_accepts_random_strategy(connection_mock):
+    connection_mock.connect.return_value = "Test"
+    connection_provider = DriverConnectionProvider(connection_mock.connect)
 
-        assert provider == default_provider_mock
+    assert connection_provider.accepts_strategy(HostRole.READER, "random") is True
+    assert connection_provider.accepts_strategy(HostRole.READER, "other") is False
 
-    def test_manager_provides_set_provider(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock: ConnectionProvider = MagicMock()
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        ConnectionProviderManager.set_connection_provider(set_provider_mock)
-        provider = connection_provider_manager.get_connection_provider(self.host_info, self.properties)
+def test_provider_returns_host_info(connection_mock):
+    connection_mock.connect.return_value = "Test"
+    connection_provider = DriverConnectionProvider(connection_mock.connect)
 
-        assert provider == set_provider_mock
+    host_info_list = [HostInfo("localhost", role=HostRole.WRITER), HostInfo("other", role=HostRole.READER)]
+    host_info = connection_provider.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
 
-    def test_manager_defaults_when_host_not_supported(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock.accepts_host_info.return_value = False
+    assert host_info.host == "localhost"
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        ConnectionProviderManager.set_connection_provider(set_provider_mock)
-        provider = connection_provider_manager.get_connection_provider(self.host_info, self.properties)
 
-        assert provider == default_provider_mock
+def test_provider_returns_connection(connection_mock):
+    host_info = HostInfo("localhost", 1234)
+    properties = Properties({"test_prop": 5})
 
-    def test_manager_accepts_strategy(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock.accepts_strategy.return_value = True
-        default_provider_mock.accepts_strategy.return_value = True
+    connection_mock.connect.return_value = "Test"
+    connection_provider = DriverConnectionProvider(connection_mock.connect)
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        ConnectionProviderManager.set_connection_provider(set_provider_mock)
-        result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
-        assert result
+    connection_provider.connect(host_info, properties)
 
-        set_provider_mock.accepts_strategy.return_value = False
-        result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
-        assert result
+    connection_mock.connect.assert_called_with(test_prop=5, host="localhost", port="1234")
 
-        default_provider_mock.accepts_strategy.return_value = False
-        result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
-        assert not result
 
-    def test_manager_get_host_info_by_strategy(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock: ConnectionProvider = MagicMock()
+def test_manager_provides_default_provider(connection_mock, default_provider_mock, host_info, properties):
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    provider = connection_provider_manager.get_connection_provider(host_info, properties)
 
-        set_provider_mock.accepts_strategy.return_value = True
+    assert provider == default_provider_mock
 
-        host_info_list = [HostInfo("localhost", role=HostRole.WRITER), HostInfo("other", role=HostRole.WRITER)]
-        set_provider_mock.get_host_info_by_strategy.return_value = host_info_list[0]
-        default_provider_mock.get_host_info_by_strategy.return_value = host_info_list[1]
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        ConnectionProviderManager.set_connection_provider(set_provider_mock)
-        host_info = connection_provider_manager.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
-        assert host_info.host == "localhost"
+def test_manager_provides_set_provider(connection_mock, default_provider_mock, set_provider_mock, host_info,
+                                       properties):
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    ConnectionProviderManager.set_connection_provider(set_provider_mock)
+    provider = connection_provider_manager.get_connection_provider(host_info, properties)
 
-        set_provider_mock.accepts_strategy.return_value = False
-        host_info = connection_provider_manager.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
-        assert host_info.host == "other"
+    assert provider == set_provider_mock
 
-    def test_release_resources(self):
-        default_provider_mock: ConnectionProvider = MagicMock()
-        set_provider_mock: ConnectionProvider = MagicMock(spec=CanReleaseResources)
 
-        connection_provider_manager = ConnectionProviderManager(default_provider_mock)
-        ConnectionProviderManager.set_connection_provider(set_provider_mock)
-        ConnectionProviderManager.release_resources()
+def test_manager_defaults_when_host_not_supported(connection_mock, default_provider_mock, set_provider_mock, host_info,
+                                                  properties):
+    set_provider_mock.accepts_host_info.return_value = False
 
-        connection_provider_manager._conn_provider.release_resources.assert_called_once()
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    ConnectionProviderManager.set_connection_provider(set_provider_mock)
+    provider = connection_provider_manager.get_connection_provider(host_info, properties)
+
+    assert provider == default_provider_mock
+
+
+def test_manager_accepts_strategy(connection_mock, default_provider_mock, set_provider_mock):
+    set_provider_mock.accepts_strategy.return_value = True
+    default_provider_mock.accepts_strategy.return_value = True
+
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    ConnectionProviderManager.set_connection_provider(set_provider_mock)
+    result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
+    assert result
+
+    set_provider_mock.accepts_strategy.return_value = False
+    result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
+    assert result
+
+    default_provider_mock.accepts_strategy.return_value = False
+    result = connection_provider_manager.accepts_strategy(HostRole.WRITER, "random")
+    assert not result
+
+
+def test_manager_get_host_info_by_strategy(connection_mock, default_provider_mock, set_provider_mock):
+    set_provider_mock.accepts_strategy.return_value = True
+
+    host_info_list = [HostInfo("localhost", role=HostRole.WRITER), HostInfo("other", role=HostRole.WRITER)]
+    set_provider_mock.get_host_info_by_strategy.return_value = host_info_list[0]
+    default_provider_mock.get_host_info_by_strategy.return_value = host_info_list[1]
+
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    ConnectionProviderManager.set_connection_provider(set_provider_mock)
+    host_info = connection_provider_manager.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
+    assert host_info.host == "localhost"
+
+    set_provider_mock.accepts_strategy.return_value = False
+    host_info = connection_provider_manager.get_host_info_by_strategy(host_info_list, HostRole.WRITER, "random")
+    assert host_info.host == "other"
+
+
+def test_release_resources(connection_mock, default_provider_mock, set_provider_mock):
+    connection_provider_manager = ConnectionProviderManager(default_provider_mock)
+    ConnectionProviderManager.set_connection_provider(set_provider_mock)
+    ConnectionProviderManager.release_resources()
+
+    connection_provider_manager._conn_provider.release_resources.assert_called_once()
