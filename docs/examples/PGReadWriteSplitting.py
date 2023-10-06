0f14ca3c60a65176f28af773ecd3ef1429d2df2f
Aaron Congo <Aaron.Congo@improving.com>
2023-10-06 16:47:36 -0700

    Add documentation for read-write splitting plugin

---
+++ docs/examples/PGReadWriteSplitting.py
@@ -0,0 +1,123 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Dict
+
+if TYPE_CHECKING:
+    from aws_wrapper.hostinfo import HostInfo
+    from aws_wrapper.pep249 import Connection
+
+import psycopg
+
+from aws_wrapper import AwsWrapperConnection
+from aws_wrapper.connection_provider import (
+    ConnectionProviderManager, SqlAlchemyPooledConnectionProvider)
+from aws_wrapper.errors import (FailoverFailedError, FailoverSuccessError,
+                                TransactionResolutionUnknownError)
+
+
+def configure_pool(host_info: HostInfo, props: Dict[str, Any]) -> Dict[str, Any]:
+    return {"pool_size": 5}
+
+
+def get_pool_key(host_info: HostInfo, props: Dict[str, Any]) -> str:
+    # Include the URL, user, and database in the connection pool key so that a new
+    # connection pool will be opened for each different instance-user-database combination.
+    url = host_info.url
+    user = props["username"]
+    db = props["dbname"]
+    return f"{url}{user}{db}"
+
+
+def configure_initial_session_states(conn: Connection):
+    awscursor = conn.cursor()
+    awscursor.execute("SET TIME ZONE 'UTC'")
+
+
+def execute_queries_with_failover_handling(conn: Connection, sql: str):
+    try:
+        cursor = conn.cursor()
+        return cursor.execute(sql)
+
+    except FailoverSuccessError:
+        # Query execution failed and AWS Advanced Python Driver successfully failed over to an available instance.
+        # https://github.com/awslabs/aws-advanced-python-wrapper/blob/main/docs/using-the-python-driver/using-plugins/UsingTheFailoverPlugin.md#FailoverFailedError---successful-failover
+
+        # The old cursor is no longer reusable and the application needs to reconfigure sessions states.
+        configure_initial_session_states(conn)
+        # Retry query
+        cursor = conn.cursor()
+        return cursor.execute(sql)
+
+    except FailoverFailedError as e:
+        # User application should open a new connection, check the results of the failed transaction and re-run it if needed. See:
+        # https://github.com/awslabs/aws-advanced-python-wrapper/blob/main/docs/using-the-python-driver/using-plugins/UsingTheFailoverPlugin.md#FailoverFailedError---unable-to-establish-sql-connection
+        raise e
+
+    except TransactionResolutionUnknownError as e:
+        # User application should check the status of the failed transaction and restart it if needed. See:
+        # https://github.com/awslabs/aws-advanced-python-wrapper/blob/main/docs/using-the-python-driver/using-plugins/UsingTheFailoverPlugin.md#TransactionResolutionUnknownError---transaction-resolution-unknown
+        raise e
+
+
+if __name__ == "__main__":
+    params = {
+        "host": "database.cluster-xyz.us-east-1.rds.amazonaws.com",
+        "dbname": "postgres",
+        "user": "john",
+        "password": "pwd",
+        "plugins": "read_write_splitting,failover,host_monitoring",
+        "wrapper_dialect": "aurora-pg",
+        "autocommit": True
+    }
+
+    """
+    Optional: configure read-write splitting to use internal connection pools.
+    The arguments passed to SqlAlchemyConnectionProvider are optional, see  UsingTheReadWriteSplittingPlugin.md
+    for more info.
+    """
+    provider = SqlAlchemyPooledConnectionProvider(configure_pool, get_pool_key)
+    ConnectionProviderManager.set_connection_provider(provider)
+
+    """ Setup step: open connection and create tables - uncomment this section to create table and test values """
+    with AwsWrapperConnection.connect(psycopg.Connection.connect, **params) as conn:
+        configure_initial_session_states(conn)
+        execute_queries_with_failover_handling(
+            conn, "CREATE TABLE IF NOT EXISTS bank_test (id int primary key, name varchar(40), account_balance int)")
+        execute_queries_with_failover_handling(
+            conn, "INSERT INTO bank_test VALUES (0, 'Jane Doe', 200), (1, 'John Smith', 200)")
+
+    """ Example step: open connection and perform transaction """
+    with AwsWrapperConnection.connect(psycopg.Connection.connect, **params) as conn, conn.cursor() as cursor:
+        configure_initial_session_states(conn)
+
+        execute_queries_with_failover_handling(
+            conn, "UPDATE bank_test SET account_balance=account_balance - 100 WHERE name='Jane Doe'")
+        execute_queries_with_failover_handling(
+            conn, "UPDATE bank_test SET account_balance=account_balance + 100 WHERE name='John Smith'")
+
+        # Internally switch to a reader connection
+        conn.read_only = True
+        for i in range(2):
+            cursor = execute_queries_with_failover_handling(conn, f"SELECT * FROM bank_test WHERE id = {i}")
+            results = cursor.fetchall()
+            for record in results:
+                print(record)
+
+        execute_queries_with_failover_handling(conn, "DROP TABLE bank_test")
+
+    """ If connection pools were enabled, close them here """
+    ConnectionProviderManager.release_resources()
