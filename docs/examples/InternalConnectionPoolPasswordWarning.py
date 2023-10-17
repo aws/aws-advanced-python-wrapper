fd1439191eed2cd1ac265c3d3bf8d49b5c43a8b5
Aaron Congo <Aaron.Congo@improving.com>
2023-10-17 15:27:20 -0700

    Update module name in r/w splitting examples

--- docs/examples/InternalConnectionPoolPasswordWarning.py
+++ docs/examples/InternalConnectionPoolPasswordWarning.py
@@ -13,9 +13,12 @@
 #  limitations under the License.
 
 import psycopg
-from aws_wrapper import AwsWrapperConnection
-from aws_wrapper.connection_provider import (
-    ConnectionProviderManager, SqlAlchemyPooledConnectionProvider)
+
+from aws_advanced_python_wrapper import AwsWrapperConnection
+from aws_advanced_python_wrapper.connection_provider import \
+    ConnectionProviderManager
+from aws_advanced_python_wrapper.sql_alchemy_connection_provider import \
+    SqlAlchemyPooledConnectionProvider
 
 if __name__ == "__main__":
     params = {
