92fd05b5d814f59eb162384a85170ea4ed2b6cd2
Karen Chen <karen.chen@improving.com>
2024-10-14 19:29:55 -0700

    fix: type errors caused by older python versions

--- aws_advanced_python_wrapper/utils/rdsutils.py
+++ aws_advanced_python_wrapper/utils/rdsutils.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import annotations
+
 from re import Match, search, sub
 from typing import Dict, Optional
 
