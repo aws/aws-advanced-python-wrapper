4319ef2147c8da343a337248819edd43a9b1d4bc
aaron-congo <aaron.congo@improving.com>
2025-06-13 10:19:50 -0700

    Attempt to fix build

--- aws_advanced_python_wrapper/utils/concurrent.py
+++ aws_advanced_python_wrapper/utils/concurrent.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import ItemsView
+    from typing import ItemsView
 
 from threading import Lock
 from typing import Callable, Generic, KeysView, List, Optional, TypeVar
