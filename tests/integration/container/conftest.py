cd55de811e1b003d64d3a633166f674c44f330a3
Favian Samatha <faviansamatha@yahoo.com>
2026-01-12 20:14:16 -0800

    fix: move test_weight_rrandom_host_selector.py to unit test folder

--- tests/integration/container/conftest.py
+++ tests/integration/container/conftest.py
@@ -109,7 +109,12 @@ def pytest_runtest_setup(item):
                 logger.warning("conftest.ExceptionWhileObtainingInstanceIDs", ex)
                 instances = list()
 
-            sleep(5)
+            # Only sleep if condition is still not met
+            if (len(instances) < request.get_num_of_instances()
+                or len(instances) == 0
+                or not rds_utility.is_db_instance_writer(instances[0])) and (
+                    timeit.default_timer() - start_time) < 300:
+                sleep(5)
 
         assert len(instances) > 0
         current_writer = instances[0]
